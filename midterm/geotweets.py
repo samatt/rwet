feccdde1da5f3173711603b6a9cc88a5f77d5b46
Surya Mattu <surya@suryamattu.com>
2013-03-09 19:47:42 -0500

    Added dictionary with tweet, modifeied geotweets

--- midterm/geotweets.py
+++ midterm/geotweets.py
@@ -7,10 +7,14 @@
 #-----------------------------------------------------------------------
 import json
 from twitter import *
+import sys
+import csv
+import time
 
 # create twitter API object
 twitter = Twitter()
 tweets =  dict()
+search = 'journey'
 oauth = OAuth(
 	'935447275-f7CdYLCF13kby9H1cUwlYK4ZUR9Jeo4IoyMpvZ6h', 
 	'giSYuMZb1cPH4YFUJVvBRVjmzyahKyWc7INyBDlXeQ', 
@@ -20,32 +24,65 @@ oauth = OAuth(
                
 twitter = Twitter(domain='api.twitter.com',
                   auth=oauth,
-                  api_version='1.1',
-                  format='json')
-#             
-#twitter = Twitter(domain='api.twitter.com',
-#                  auth=oauth,
-#                  api_version='1')
+                  api_version='1.1')
+
 
 # perform a basic search 
 # twitter API docs: https://dev.twitter.com/docs/api/1/get/search
-query = twitter.search.tweets(q = "new cross",geocode = "51.474144,-0.035401,1km")
+ #until Returns tweets generated before the given date. Date should be formatted as YYYY-MM-DD.
 
+# open a file to append, and create a CSV writer object
+#csvfile = file("test.csv", "a")
+output = file("tweets.txt","a")
+#csvwriter = csv.writer(csvfile)
+#row = [ "user", "text", "place", "type"]
+
+#for pagenum in range(1, 11):
+query = twitter.search.tweets(q = search,geocode = "40.770496,-73.989089,10mi",rrp=100)
+#query = twitter.search.tweets(q = "the",rpp=100,lang='en')
 data = query.items()
 #data = json.loads(query.items())
 #print tweets
 
 for tuples in data:	
 	for lists in  tuples:
-		#print item
-		if (type(lists)==list):
+		print type(lists)
+
+		if (type(lists)==dict):
+			print 'No of tweets: %d' % lists[u'count'] 
+			#print lists
+
+		elif (type(lists)==list): 
 			
 			for dicts in lists:
-				#print dicts.keys()
-				if dicts['coordinates']:
-				#print dic['user'] 
-					print dicts['place'] 
-				#print dic[u'coordinates'] 
-			
+				#print dicts['geo']
+				if dicts['text']:
+					#print dicts['user'] 
+					user = dicts['user'] ['name']
+					user = user.encode('ascii', 'replace')
+					text =  dicts['text'] 
+					text = text.encode('ascii', 'replace')
+					#print text
+					if dicts[u'place']:
+						print dicts['place']['name']
+						place = dicts['place']['name']
+						tweets[place] = text
+						placeType = dicts['place']['place_type']
+						#row = [ user, text, place, placeType ]
+						row = "%s|%s|%s|%s|%s \n"%(search,user,text,place,placeType)
+						#user+"|"+text+"|"+place+"|"+placeType 
+						output.write(row)
+						#csvwriter.writerow(row)
+					else:
+						#row = [ user, text]
+						#row = +user+"|"+text
+						row = "%s|%s|%s \n"%(search,user,text)
+						output.write(row)
+						#csvwriter.writerow(row)
+					
 
+					#print dicts[u'text'] 
+#print "done page: %d" % (pagenum)			
+output.close()
+#csvfile.close()
 
---
+++ midterm/tweetdict.py
@@ -0,0 +1,32 @@
+import sys
+import random
+
+tweets = dict()
+keys = list()
+source_file = sys.argv[1] # first argument passed on command line
+
+for line in open(source_file):
+  line = line.strip()
+  lists = list()
+  #split into sentences 
+  sentences = line.split("|")
+  # for sentence in sentences:
+  #print sentences
+  if sentences[0] in keys :
+    tweets[sentences[0]].append([sentences[1],sentences[2]]) 
+    print sentences[0]
+  else:  
+    keys.append(sentences[0])
+    tweets[sentences[0]] = []
+    tweets[sentences[0]].append([sentences[1],sentences[2]])
+
+
+  #tweets[sentences[1]] = sentences[0],sentences[2]
+  
+print tweets
+#for pagenum in range(1, 11):
+#  print tweets[keys[0]][5]
+
+
+
+
